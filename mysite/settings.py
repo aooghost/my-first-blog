80f2a70b6bb523dfb8a1625a3a4b588a49a6a49c
aooghost <fr.nikon@gmail.com>
2017-03-29 13:32:04 +0100

    settings.py email settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-DEFAULT_FROM_EMAIL = 'testing@example.com'
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-EMAIL_USE_TLS = False
-EMAIL_PORT = 1025
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_HOST_USER='fr.nikon@gmail.com'
+EMAIL_HOST_PASSWORD='151188_Na'
+EMAIL_USE_TLS=True
