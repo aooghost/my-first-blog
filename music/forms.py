268303df3f10f657dcdbc8610cf97b880d4fdd45
aooghost <fr.nikon@gmail.com>
2017-03-29 16:57:57 +0100

    email form frontend edit

--- music/forms.py
+++ music/forms.py
@@ -13,6 +13,6 @@ class ContactForm(forms.Form):
     # the new bit we're adding
     def __init__(self, *args, **kwargs):
         super(ContactForm, self).__init__(*args, **kwargs)
-        self.fields['contact_name'].label = "Your name:"
-        self.fields['contact_email'].label = "Your email:"
-        self.fields['content'].label = "What do you want to say?"
+        self.fields['contact_name'].label = "Name:"
+        self.fields['contact_email'].label = "Email:"
+        self.fields['content'].label = "Message   "
